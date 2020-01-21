19cc600e1f8fc25173ab1cefeb30cee50955a285
Bridget Asser <bridgetasser@gmail.com>
2020-01-21 14:56:13 -0500

    data model updated and admin view

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Reading
+from .models import Reading, Read
 
 # Register your models here
 admin.site.register(Reading)
+admin.site.register(Read)
--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,39 @@
 from django.db import models
-
 from django.urls import reverse
+from datetime import date
+
+CHAPS = (
+        ('1', '<1 Chapter'),
+        ('2', '1/2 Chapters'),
+        ('3', '3+ Chapters')
+)
 
 class Reading(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
-    pages = models.IntegerField()
+        title = models.CharField(max_length=100)
+        author = models.CharField(max_length=100)
+        description = models.TextField(max_length=300)
+        pages = models.IntegerField()
+        # quotes = models.ManyToManyField(quote)
 
 def __str__(self):
         return self.name
 
 def get_absolute_url(self):
         return reverse('detail', kwargs={'reading_id': self.id})
+
+class Read(models.Model):
+        date = models.DateField('Reading date')
+        chapter = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=CHAPS,
+        # set the default value for meal to be 'B'
+        default=CHAPS[0][0]
+)
+
+reading = models.ForeignKey(Reading, on_delete=models.CASCADE)
+
+def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_chapter_display()} on {self.date}"
+
