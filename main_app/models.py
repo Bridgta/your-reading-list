090e056cfafd3b9114f22c582b5c43f119eeeb85
Bridget Asser <bridgetasser@gmail.com>
2020-01-20 21:56:22 -0500

    defined routes, set up templates, defined view functions and so much more

---
+++ main_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Reading(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    pages = models.IntegerField()
+
+
+# def __str__(self):
+#         return self.name
